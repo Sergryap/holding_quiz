326c93da0ccaead1116e57d127f3a349c7ee97a5
sergryap <rs1180@mail.ru>
2022-11-05 12:49:03 +0500

    The questions-answers dictionary has been got

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import os
+import re
+from pprint import pprint
 
 
-def read_files_quiz(path):
-    files_quiz = os.listdir('quiz-questions')
+def create_quiz_questions(path):
+    quiz = {}
+    files_quiz = os.listdir(path)
+    next_answer = False
+    number = 0
+    pattern = re.compile(r'^(Вопрос|Ответ)\s?(\d*):\n([\s\S]+)')
+
     for file_quiz in files_quiz:
-        with open(os.path.join(os.getcwd(), path, file_quiz), 'r', encoding='koi8-r') as file:
-            file_contents = file.read()
-        print(file_contents)
+        with open(os.path.join(os.getcwd(), path, file_quiz), 'r', encoding='koi8-r') as f:
+            file = f.read()
+        for block in re.split(r'\n{2}', file):
+            info = pattern.search(block)
+            if info:
+                if info.group(2).isdigit():
+                    question = info.group(3)
+                    next_answer = True
+                elif next_answer:
+                    next_answer = False
+                    number += 1
+                    answer = info.group(3)
+                    quiz.update({
+                        number: {
+                            'question': question,
+                            'answer': answer,
+                        }
+                    })
+    return quiz
 
 
 if __name__ == '__main__':
     PATH_QUIZ = 'quiz-questions'
-    read_files_quiz(PATH_QUIZ)
+    pprint(create_quiz_questions(PATH_QUIZ))
