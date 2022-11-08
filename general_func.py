51baeaf5122ca002610fa1bfa022f6f3bda957c2
sergryap <rs1180@mail.ru>
2022-11-08 20:34:53 +0500

    Modified comment to get_redis_user_data func

--- general_func.py
+++ general_func.py
@@ -7,7 +7,7 @@ def get_redis_user_data(user_id, redis_connect):
     Получение данных о пользователе из Redis
     answer_correct - ответ на текущий волпрос
     number_question - номер текущего вопроса
-    all_numbers - списиок вопросов, на которые пользователь уже отвечал
+    all_numbers - список вопросов, на которые пользователь уже отвечал
     waiting_answer - значение True указывает на состояние ожидания ответа от пользователя (для Vk)
     """
     redis_user = json.loads(redis_connect.get(user_id))
